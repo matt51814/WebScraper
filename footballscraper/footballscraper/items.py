49f69627e702a71402bfbaea73aa2d33c3ebf2a1
matt51814 <matthawthorn@hotmail.com>
2024-11-22 22:00:22 +0000

    feat(spiders): scrape standings spider

--- footballscraper/footballscraper/items.py
+++ footballscraper/footballscraper/items.py
@@ -4,9 +4,20 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from dataclasses import dataclass
 
 
 class FootballscraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+@dataclass
+class StandingItem:
+    pos: str
+    team: str
+    matches: int
+    points: int
+    plus_and_minus: int
+    goals: str
