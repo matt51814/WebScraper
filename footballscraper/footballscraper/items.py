f13b7dac825d35b7f2111f3370560a64ad7d79a5
matt51814 <matthawthorn@hotmail.com>
2024-11-23 12:27:35 +0000

    feat(items): define dataclasses for each table

--- footballscraper/footballscraper/items.py
+++ footballscraper/footballscraper/items.py
@@ -5,6 +5,7 @@
 
 import scrapy
 from dataclasses import dataclass
+from datetime import datetime
 
 
 class FootballscraperItem(scrapy.Item):
@@ -21,3 +22,33 @@ class StandingItem:
     points: int
     plus_and_minus: int
     goals: str
+
+
+@dataclass
+class RecentMatchesItem:
+    date: datetime
+    home_team: str
+    away_team: str
+    home_score: int
+    away_score: int    
+    odds: str    
+
+@dataclass
+class NextMatchesItem:
+    date: datetime
+    home_team: str
+    away_team: str 
+    odds: str    
+
+
+@dataclass
+class ScorersItem:
+    team: str
+    player: str
+    scorer_points: int
+    goals: int
+    assists: int
+    soccer_points_per_match: float
+    goals_per_match: float
+    avg_minutes_to_score_or_assist: float
+    avg_minutes_to_score: float
